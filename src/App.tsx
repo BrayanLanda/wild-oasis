abc15839143ff9fe74b7f00f087545e2e61f7a76
Brayan Acosta <landakkpp@gmail.com>
2024-09-24 19:54:23 -0500

    add component basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
-import styled from "styled-components"
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Heading from "./ui/Heading";
+import Input from "./ui/Input";
+import Row from "./ui/Row";
 
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-`
+const StyledApp = styled.main`
+  padding: 20px;
+`;
 
 export default function App() {
   return (
-    <div>
-      <H1>
-        The Wild Oasis
-      </H1>
-    </div>
-  )
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row as="horizontal">
+          <Heading as="h1">The Wild Oasis</Heading>
+          <div>
+            <Heading as="h2">Check in and out</Heading>
+            <Button onClick={() => alert("Hello")}>Check</Button>
+            <Button onClick={() => alert("Check out")}>Check out</Button>
+          </div>
+        </Row>
+
+        <Row as="vertical">
+          <Heading as="h3">Form</Heading>
+          <form>
+            <Input type="number" placeholder="Number" />
+            <Input type="number" placeholder="Number" />
+          </form>
+        </Row>
+      </StyledApp>
+    </>
+  );
 }
