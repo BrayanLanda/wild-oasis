906a02af675833d32646865d79a119b967779f82
Brayan Acosta <landakkpp@gmail.com>
2024-11-20 22:35:01 -0500

    add conection with supabase

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import Dashboard from "./pages/Dashboard"
 import GlobalStyles from "./styles/GlobalStyles"
 import AppLayout from "./ui/AppLayout"
+import Cabins from "./pages/Cabins"
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Route element={<AppLayout />}> 
           <Route index element={<Navigate replace to="dashboard" />} />
           <Route path="dashboard" element={<Dashboard />} />
-          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="cabins" element={<Cabins />} />
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="dashboard" element={<Dashboard />} />
---
+++ src/services/supabase.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://jclsfsglyjllujgtiasy.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpjbHNmc2dseWpsbHVqZ3RpYXN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ4NDU0OTMsImV4cCI6MjA0MDQyMTQ5M30.U0SM9iEibL5PJLjpgT8f4j0VrkXzraHGn_otAdzsZ94';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
